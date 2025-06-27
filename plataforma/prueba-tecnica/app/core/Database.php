fe9f202fe11577f15842915cc38a4279dd43ddee
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-06-27 09:01:05 -0500

    refactor: implement IDatabase interface and extract connection logic into a separate method for improved code organization

--- plataforma/prueba-tecnica/app/core/Database.php
+++ plataforma/prueba-tecnica/app/core/Database.php
@@ -1,18 +1,23 @@
 <?php
 
-class Database
-{
+class Database implements IDatabase
+{ 
     private $conn;
 
     public function __construct()
     {
         try {
-            $this->conn = new PDO("mysql:host=" . DB_HOSTNAME . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connect();
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
         }
+    }
+
+    public function connect()
+    {
+        $this->conn = new PDO("mysql:host=" . DB_HOSTNAME . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function getConnection()
