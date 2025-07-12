ef724a31127d0042a999a42de0830976adf8efea
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-11 19:33:17 -0500

    refactor: Refactor web routes to use controllers for homepage and login functionality

--- worldskills/WeBook/routes/web.php
+++ worldskills/WeBook/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ViewController::class, 'index']);
+
 
 Route::post('/login', [AuthController::class, 'login']);
