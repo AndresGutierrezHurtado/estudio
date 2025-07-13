ed83da1f96038fb9679f53dfe72fe087db18ca04
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-12 23:42:19 -0500

    feat: add web routes

--- worldskills/studyly/routes/web.php
+++ worldskills/studyly/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Auth routes
+Route::get('/', [ViewController::class, 'index']);
+Route::get('/register', [ViewController::class, 'register']);
+
+// Student Routes
+Route::get('/calls', [ViewController::class, 'calls']);
