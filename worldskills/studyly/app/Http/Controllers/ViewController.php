9188bef465bd0eee9948a6d62ad41e203e551dce
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-13 21:36:14 -0500

    feat: add new controllers and improve web routes structure

--- worldskills/studyly/app/Http/Controllers/ViewController.php
+++ worldskills/studyly/app/Http/Controllers/ViewController.php
@@ -27,6 +27,8 @@ class ViewController extends Controller
 
     public function calls()
     {
+        $userSession = Auth::user()->with('role', 'student');
+
         $search = request()->get('search', '');
         $type = request()->get('search', '');
 
@@ -41,6 +43,26 @@ class ViewController extends Controller
         $calls = $calls->get()->toArray();
 
         dd($calls);
-        return view('app.calls', compact('calls'));
+        return view('app.calls', compact('calls', 'userSession'));
+    }
+
+    public function callsManagement()
+    {
+        $userSession = Auth::user()->with('role', 'student');
+
+        $search = request()->get('search', '');
+        $type = request()->get('search', '');
+
+        $calls = Call::where(function ($query) use ($search) {
+            $query->where('call_name', 'LIKE', "%$search%");
+            $query->orWhere('call_description', 'LIKE', "%$search%");
+            $query->orWhere('call_type', 'LIKE', "%$search%");
+        });
+        $calls = $calls->where('call_start', '<', now());
+        $calls = $calls->where('call_end', '>', now());
+
+        $calls = $calls->get()->toArray();
+
+        return view('app.admin.calls', compact('calls', 'calls', 'userSession'));
     }
 }
--- worldskills/studyly/routes/web.php
+++ worldskills/studyly/routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
-use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ViewController;
+use App\Http\Controllers\AuthController;
+
 // Auth routes
-Route::get('/', [ViewController::class, 'index']);
-Route::get('/register', [ViewController::class, 'register']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/', [ViewController::class, 'index']);
+    Route::get('/login', [ViewController::class, 'index']);
+    Route::get('/register', [ViewController::class, 'register']);
+});
 
 // Student Routes
-Route::get('/calls', [ViewController::class, 'calls']);
+Route::middleware(['auth', 'role:1'])->group(function () {
+    Route::get('/calls', [ViewController::class, 'calls']);
+});
+
+// Admin routes
+Route::middleware(['auth', 'role:2'])->group(function () {
+    Route::get('/calls', [ViewController::class, 'callsManagement']);
+});
+
+// Logic Routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
