3cb44d6976d59c12b76a5e3dd885c08dad4cde0e
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-16 21:29:01 -0500

    feat: add routes for managing countries and medals in web.php

--- worldskills/olimpic-medals/routes/web.php
+++ worldskills/olimpic-medals/routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
-use App\Http\Controllers\ViewController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\MedalController;
+use App\Http\Controllers\ViewController;
+
+// View Routes
 Route::get('/', [ViewController::class, 'index']);
 Route::get('/countries', [ViewController::class, 'countries']);
 Route::get('/medals', [ViewController::class, 'medals']);
+
+// ----- LOGIC ROUTES ----- //
+
+// Countries Routes
+Route::post('/countries', [CountryController::class, 'store']);
+Route::put('/countries/{id}', [CountryController::class, 'update']);
+Route::delete('/countries/{id}', [CountryController::class, 'destroy']);
+
+// Medals Routes
+Route::post('/medals', [MedalController::class, 'store']);
+Route::put('/medals/{id}', [MedalController::class, 'update']);
+Route::delete('/medals/{id}', [MedalController::class, 'destroy']);
