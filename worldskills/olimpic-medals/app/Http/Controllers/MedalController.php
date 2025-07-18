e7b6f5df02a5d8aab6573cbd1f564800e2e31459
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-17 21:46:57 -0500

    fix: fix medal controller

--- worldskills/olimpic-medals/app/Http/Controllers/MedalController.php
+++ worldskills/olimpic-medals/app/Http/Controllers/MedalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Medal;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 
@@ -14,7 +15,18 @@ class MedalController extends Controller
     public function store(Request $request)
     {
         try {
-            // 
+            $request->validate(
+                [
+                    'country_id' => 'required|integer',
+                    'medal_sport' => 'required|string',
+                    'medal_year' => 'required|integer|min:1900|max:2025',
+                    'medal_type' => 'required|in:gold,silver,bronze',
+                ],
+            );
+
+            $medal = Medal::create($request->all());
+
+            return redirect("/medals?search={$medal->medal_sport}");
         } catch (ValidationException $e) {
             return redirect()->back()->withErrors($e->errors())->withInput($request->all());
         } catch (\Exception $e) {
@@ -27,7 +39,18 @@ class MedalController extends Controller
     public function update(Request $request, string $id)
     {
         try {
-            // 
+            $request->validate(
+                [
+                    'country_id' => 'required|integer',
+                    'medal_sport' => 'required|string',
+                    'medal_year' => 'required|integer|min:1900|max:2025',
+                    'medal_type' => 'required|in:gold,silver,bronze',
+                ],
+            );
+
+            $medal = Medal::where('medal_id', $id)->update($request->only('country_id', 'medal_sport', 'medal_year', 'medal_type'));
+
+            return redirect("/medals?search={$medal->medal_sport}");
         } catch (ValidationException $e) {
             return redirect()->back()->withErrors($e->errors())->withInput($request->all());
         } catch (\Exception $e) {
@@ -41,7 +64,15 @@ class MedalController extends Controller
     public function destroy(string $id)
     {
         try {
-            // 
+            $existingMedal = Medal::find($id);
+
+            if (!$existingMedal) {
+                throw new \Exception("La medalla no existe");
+            }
+
+            Medal::destroy($id);
+
+            return redirect("/medals");
         } catch (\Exception $e) {
             return redirect()->back()->with('error', $e->getMessage());
         }
