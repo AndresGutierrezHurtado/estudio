aee6beb13e6151518e838403570069b48116dda4
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-18 21:28:02 -0500

    feat: add home view

--- worldskills/olimpic-medals/resources/views/home.blade.php
+++ worldskills/olimpic-medals/resources/views/home.blade.php
@@ -6,6 +6,34 @@
     <section class="w-full px-5">
         <div class="w-full max-w-[1200px] mx-auto py-10 space-y-8">
             <h1 class="text-4xl font-extrabold">Historial de medallas olímpicas de cada país</h1>
+            <div class="join join-vertical bg-base-100 w-full">
+                @foreach ($countries as $country)
+                    <div class="collapse collapse-arrow join-item border-base-300 border">
+                        <input type="checkbox" name="item-{{ $country['country_id'] }}" />
+                        <div class="collapse-title font-semibold capitalize">{{ $country['country_name'] }}</div>
+                        <ul class="collapse-content">
+                            @forelse ($country['medals'] as $medal)
+                                <li>
+                                    <span>
+                                        @if ($medal['medal_type'] === 'gold')
+                                            🥇 Oro
+                                        @elseif($medal['medal_type'] === 'silver')
+                                            🥈 Plata
+                                        @elseif($medal['medal_type'] === 'bronze')
+                                            🥉 Bronce
+                                        @endif
+                                    </span>
+                                    {{ $medal['medal_sport'] }} {{ $medal['medal_year'] }}
+                                </li>
+                            @empty
+                                <li>
+                                    No cuenta con medallas registradas aún...
+                                </li>
+                            @endforelse
+                        </ul>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
 @endsection
