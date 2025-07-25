b040fe16dceb48e44ad7c8a838ef5341b7ac5456
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-25 11:17:51 -0500

    feat: add CSS styles for layout and components, including reset and container styles

--- worldskills/copa-america/client/src/main.jsx
+++ worldskills/copa-america/client/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 // Entry Point
-import App from './App.jsx'
+import App from "./App.jsx";
 
-import './index.css'
+// Styles
+import "./reset.css";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
