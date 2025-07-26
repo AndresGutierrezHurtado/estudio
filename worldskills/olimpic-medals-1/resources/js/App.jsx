e924893dcb409df478eb9e52d73bc49218401e7a
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-26 11:29:04 -0500

    feat: add missing views

--- worldskills/olimpic-medals-1/resources/js/App.jsx
+++ worldskills/olimpic-medals-1/resources/js/App.jsx
@@ -7,24 +7,28 @@ import AppLayout from "./layouts/AppLayout";
 // Pages
 import Menu from "./pages/Menu";
 import Admin from "./pages/Admin";
+import Teams from "./pages/app/Teams";
+import Team from "./pages/app/Team";
+import Results from "./pages/app/Results";
+import Ranking from "./pages/app/Ranking";
+import TeamsManagement from "./pages/admin/Teams";
+import ResultsManagement from "./pages/admin/Results";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<AppLayout />}>
+                    {/* Show routes */}
                     <Route index element={<Menu />} />
-                    {/*
-                    <Route path="/teams" element={<Admin />} />
-                    <Route path="/teams/:id" element={<Admin />} />
-                    <Route path="/results" element={<Admin />} />
-                    <Route path="/ranking" element={<Admin />} />
-                    */}
+                    <Route path="/teams" element={<Teams />} />
+                    <Route path="/teams/:id" element={<Team />} />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/ranking" element={<Ranking />} />
+                    {/* Admin Routes */}
                     <Route path="/admin" element={<Admin />} />
-                    {/*
-                    <Route path="/admin/teams" element={<Admin />} />
-                    <Route path="/admin/results" element={<Admin />} />
-                    */}
+                    <Route path="/admin/teams" element={<TeamsManagement />} />
+                    <Route path="/admin/results" element={<ResultsManagement />} />
                 </Route>
             </Routes>
         </BrowserRouter>
