f257380516515404a572a3c8c7a4c8e06fdd42e1
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-26 11:03:37 -0500

    feat: define API routes for teams and plays with CRUD operations

--- worldskills/olimpic-medals-1/routes/api.php
+++ worldskills/olimpic-medals-1/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Teams routes
+Route::post('/teams', [TeamController::class, 'store']);
+Route::get('/teams', [TeamController::class, 'index']);
+Route::get('/teams/ranking', [TeamController::class, 'ranking']);
+Route::get('/teams/{id}', [TeamController::class, 'show']);
+
+// Play routes
+Route::post('/plays', [TeamController::class, 'store']);
+Route::get('/plays', [TeamController::class, 'index']);
